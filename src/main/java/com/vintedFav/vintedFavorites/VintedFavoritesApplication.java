9e0bd5ccc73c3788def7919a9a41d0523cc88fd2
maiav <maiazeldaverdier@gmail.com>
2025-11-23 12:14:02 +0100

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/vintedFav/vintedFavorites/VintedFavoritesApplication.java
@@ -0,0 +1,13 @@
+package com.vintedFav.vintedFavorites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VintedFavoritesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VintedFavoritesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vintedFav/vintedFavorites/VintedFavoritesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vintedFav.vintedFavorites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VintedFavoritesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
